03ee7a7ba855f71ef124948dd40d31d2023ad8bf
raLiinu30 <46948627+raLiinu30@users.noreply.github.com>
2019-03-22 14:32:37 -0400

    Updated from Brython Server: 3/22/2019 2:32:36 PM

--- cryptography.py
+++ cryptography.py
@@ -9,4 +9,37 @@ Write and submit a program that encrypts and decrypts user data.
 
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
+
+'''
+Enter e to encrypt, d to decrypt, or q to quit: z
+Did not understand command, try again.
+Enter e to encrypt, d to decrypt, or q to quit: e
+Message: Two plus two = Five
+Key: Lorem ipsum
++KF;B(CH=NIZ}m;R\Dt
+Enter e to encrypt, d to decrypt, or q to quit: d
+Message: +KF;B(CH=NIZ}m;R\Dt
+Key: Lorem ipsum
+Two plus two = Five
+Enter e to encrypt, d to decrypt, or q to quit: q
+Goodbye!
+'''
+
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+
+Z = input('Enter e to encrypt, d to decrypt, or q to quit: ')
+    if Z != 'e' and Z != 'd' and Z != 'q' :
+    print('Did not understand command, try again.')
+    Message = input('Message: ')
+    Key: input('Key: ')
+    for x in Message:
+    E = (associations.find(x))
+    
+
+
+
+
+
+
+
+
